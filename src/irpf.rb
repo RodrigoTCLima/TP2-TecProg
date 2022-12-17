e8fd5ab58b11e73d32572eac404a94998da53020
rodrigo <rodrigotiagocl@gmail.com>
2022-12-17 15:55:48 -0300

    Implementando cadastro de deduções usando a técnica de falsificação
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

--- src/irpf.rb
+++ src/irpf.rb
@@ -2,10 +2,13 @@ require './src/exceptions'
 
 class IRPF
   attr_accessor :valorTotalRendimentos, :rendimentosDeclarados
+  attr_accessor :valorTotalDeducoes, :deducoesDeclaradas
 
   def initialize
     @valorTotalRendimentos = 0
     @rendimentosDeclarados = []
+    @valorTotalDeducoes = 0
+    @deducoesDeclaradas = []
   end
 
   def cadastroRendimento(valorRendimento, descricaoRendimento)
@@ -13,5 +16,10 @@ class IRPF
     raise ValorRendimentoInvalidoException if valorRendimento.nil? || valorRendimento <= 0
     @valorTotalRendimentos += valorRendimento
     @rendimentosDeclarados << descricaoRendimento
+  end
+
+  def cadastroDeducao(valorDeducao, descricaoDeducao)
+    @valorTotalDeducoes += valorDeducao
+    @deducoesDeclaradas << descricaoDeducao
   end
 end

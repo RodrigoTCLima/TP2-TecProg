63077e4567844dc3c9033efe40af3d76777e9c1c
rodrigo <rodrigotiagocl@gmail.com>
2022-12-17 17:01:23 -0300

    Parametrizando testes de cadastro de dedução
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

--- test/cadastro_deducao_spec.rb
+++ test/cadastro_deducao_spec.rb
@@ -3,24 +3,28 @@ require './src/irpf.rb'
 describe 'Cadastro de dedução' do
   let(:irpf) { IRPF.new }
 
+  [ {descricao: 'Previdencia Privada', valor: 1000},
+    {descricao: 'Funpresp', valor: 2000},
+    {descricao: 'Parcela isenta de aposentadoria', valor: 4000}
+  ].each do |parametros|
   describe 'quando é realizado cadastro do dedução' do
-    let(:previdenciaPrivadaValor) { 1000 }
-    let(:previdenciaPrivadaDescricao) { 'Previdencia Privada' }
-
-    let(:funprespValor) { 2000 }
-    let(:funprespDescricao) { 'Funpresp' }
-    
     before do
-      irpf.cadastroDeducao(previdenciaPrivadaValor, previdenciaPrivadaDescricao)
-      irpf.cadastroDeducao(funprespValor, funprespDescricao)
+      irpf.cadastroDeducao(parametros[:valor], parametros[:descricao])
+      
+      @totalDeducoes ||=0
+      @totalDeducoes += parametros[:valor]
+
+      @deducoesDeclaradas ||= []
+      @deducoesDeclaradas << parametros[:descricao]
+      
     end
 
     it 'a dedução total deve ser igual ao valor cadastrado' do
-      expect(irpf.valorTotalDeducoes).to eq previdenciaPrivadaValor + funprespValor
+      expect(irpf.valorTotalDeducoes).to eq @totalDeducoes
     end
 
     it 'a lista de deduções declaradas deve conter o novo cadastro' do
-      expect(irpf.deducoesDeclaradas).to eq [previdenciaPrivadaDescricao, funprespDescricao]
+      expect(irpf.deducoesDeclaradas).to eq @deducoesDeclaradas
     end
   end
 end

903bbef5d7a7a0fba436d81f0f4277fbdc1555eb
rodrigo <rodrigotiagocl@gmail.com>
2022-12-17 16:52:39 -0300

    Adicionando duplicação no teste de cadastro de deduções
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

--- test/cadastro_deducao_spec.rb
+++ test/cadastro_deducao_spec.rb
@@ -7,16 +7,20 @@ describe 'Cadastro de dedução' do
     let(:previdenciaPrivadaValor) { 1000 }
     let(:previdenciaPrivadaDescricao) { 'Previdencia Privada' }
 
+    let(:funprespValor) { 2000 }
+    let(:funprespDescricao) { 'Funpresp' }
+    
     before do
       irpf.cadastroDeducao(previdenciaPrivadaValor, previdenciaPrivadaDescricao)
+      irpf.cadastroDeducao(funprespValor, funprespDescricao)
     end
 
     it 'a dedução total deve ser igual ao valor cadastrado' do
-      expect(irpf.valorTotalDeducoes).to eq previdenciaPrivadaValor
+      expect(irpf.valorTotalDeducoes).to eq previdenciaPrivadaValor + funprespValor
     end
 
     it 'a lista de deduções declaradas deve conter o novo cadastro' do
-      expect(irpf.deducoesDeclaradas).to eq [previdenciaPrivadaDescricao]
+      expect(irpf.deducoesDeclaradas).to eq [previdenciaPrivadaDescricao, funprespDescricao]
     end
   end
 end

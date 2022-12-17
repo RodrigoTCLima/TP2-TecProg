4814c609f47f95b043a39a72b488b41886404fa3
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-12-17 13:16:14 -0300

    Implementando cadastroRendimento usando a técnica de falsificação
    Co-authored-by: Rodrigo Lima <rodrigotiagocl@gmail.com>

--- src/irpf.rb
+++ src/irpf.rb
@@ -1,4 +1,13 @@
 class IRPF
+  attr_accessor :valorTotalRendimentos, :rendimentosDeclarados
+
   def initialize
+    @valorTotalRendimentos = 0
+    @rendimentosDeclarados = []
+  end
+
+  def cadastroRendimento(valorRendimento, descricaoRendimento)
+    @valorTotalRendimentos += valorRendimento
+    @rendimentosDeclarados << descricaoRendimento
   end
 end

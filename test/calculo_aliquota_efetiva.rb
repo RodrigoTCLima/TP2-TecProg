de956c8fd0c77a5c34b8288443b99169af1588c8
rodrigo <rodrigotiagocl@gmail.com>
2022-12-18 17:02:17 -0300

    Realizando triangulação por parametrização os testes de calculo de aliquota efetiva
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

--- test/calculo_aliquota_efetiva.rb
+++ test/calculo_aliquota_efetiva.rb
@@ -1,27 +1,21 @@
 require './src/irpf'
 
 describe 'Calculo aliquota efetiva'
-    let(:irpf) { IRPF.new }
+    let!(:irpf) { IRPF.new }
 
-    describe 'quando feito corretamente' do
-        before do
-            irpf.cadastroRendimento('Salario', 10000)
-            irpf.cadastroDeducao('Previdência Privada', 1000)
-            irpf.calculaTaxas()
-        end
-        it 'a aliquota efetiva deve ser a porcentagem do imposto sobre o rendimento total' do
-            expect(irpf.calculaAliquotaEfetiva()).to be_within(0.1).of(16.05)
-        end
-    end
-
-    describe 'quando feito corretamente' do
-        before do
-            irpf.cadastroRendimento('Salario', 7000)
-            irpf.cadastroDeducao('Previdência Privada', 1000)
-            irpf.calculaTaxas()
-        end
-        it 'a aliquota efetiva deve ser a porcentagem do imposto sobre o rendimento total' do
-            expect(irpf.calculaAliquotaEfetiva()).to be_within(0.1).of(11.15)
+    [{descricaoRend: 'Salário', rendimento: 10000, descricaoDedu: 'Funpresp', deducao: 1000, resultado:16.05},
+     {descricaoRend: 'Salário', rendimento: 7000,  descricaoDedu: 'Funpresp', deducao: 1000, resultado:11.15},
+     {descricaoRend: 'Salário', rendimento: 3000,  descricaoDedu: 'Funpresp', deducao: 500,  resultado:1.49}
+    ].each do |parametros|
+        describe 'quando feito corretamente' do
+            before do
+                irpf.cadastroRendimento(parametros[:descricaoRend], parametros[:rendimento])
+                irpf.cadastroDeducao(parametros[:descricaoDedu], parametros[:deducao])
+                irpf.calculaTaxas()
+            end
+            it 'a aliquota efetiva deve ser a porcentagem do imposto sobre o rendimento total' do
+                expect(irpf.calculaAliquotaEfetiva()).to be_within(0.1).of(parametros[:resultado])
+            end
         end
     end
 end

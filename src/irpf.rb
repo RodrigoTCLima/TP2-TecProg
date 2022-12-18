06ef582bb5067469fdf14b34195c0833fa22d7fa
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-12-18 16:26:41 -0300

    Implementando cálculo dos impostos das faixas de impostos
    Co-authored-by: Rodrigo Lima <rodrigotiagocl@gmail.com>

--- src/irpf.rb
+++ src/irpf.rb
@@ -5,14 +5,26 @@ DEDUCAO_DEPENDENTE = 189.59
 class IRPF
   attr_accessor :valorTotalRendimentos, :rendimentosDeclarados
   attr_accessor :valorTotalDeducoes, :deducoesDeclaradas
-  attr_accessor :listaDependentes
+  attr_accessor :listaDependentes, :totalImposto
+  attr_accessor :impostosPorFaixa, :baseImpostoPorFaixa
+
+  FAIXA_IMPOSTO = [
+    { faixa: 4664.68, aliquota: 27.5 },
+    { faixa: 3751.05, aliquota: 22.5 },
+    { faixa: 2826.65, aliquota: 15 },
+    { faixa: 1903.99, aliquota: 7.5 },
+  ]
 
   def initialize
     @valorTotalRendimentos = 0
-    @rendimentosDeclarados = []
     @valorTotalDeducoes = 0
+    @totalImposto = 0
+
+    @rendimentosDeclarados = []
     @deducoesDeclaradas = []
     @listaDependentes = []
+    @impostosPorFaixa = []
+    @baseImpostoPorFaixa = []
   end
 
   def cadastroRendimento(valorRendimento, descricaoRendimento)
@@ -43,5 +55,30 @@ class IRPF
     raise NomeEmBrancoException if nomeDependente.nil? || nomeDependente.empty?
     @valorTotalDeducoes += DEDUCAO_DEPENDENTE
     @listaDependentes << nomeDependente
+  end
+
+  def calculaTaxas
+    calculoBase = @valorTotalRendimentos - @valorTotalDeducoes
+
+    FAIXA_IMPOSTO.each do |imposto|
+      if calculoBase > imposto[:faixa]
+        baseImpostoFaixaN = calculoBase - imposto[:faixa]
+        calculoFaixaN = (baseImpostoFaixaN) * imposto[:aliquota] / 100
+        calculoBase = imposto[:faixa]
+
+        @totalImposto += calculoFaixaN
+        @baseImpostoPorFaixa << baseImpostoFaixaN
+        @impostosPorFaixa << calculoFaixaN
+      else
+        @baseImpostoPorFaixa << 0
+        @impostosPorFaixa << 0
+      end
+    end
+
+    @baseImpostoPorFaixa << FAIXA_IMPOSTO.last[:faixa]
+    @impostosPorFaixa << 0
+
+    @impostosPorFaixa.reverse!
+    @baseImpostoPorFaixa.reverse!
   end
 end

ed09c96878456a37eb49bb3c4411fd8946118d66
rodrigo <rodrigotiagocl@gmail.com>
2022-12-18 00:29:32 -0300

    Adicionando testes de falsificação para dedução por previdência oficial, pensão alimentícia e dependentes
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

--- test/cadastro_deducao_spec.rb
+++ test/cadastro_deducao_spec.rb
@@ -3,6 +3,7 @@ require './src/irpf.rb'
 describe 'Cadastro de dedução' do
   let(:irpf) { IRPF.new }
 
+  # cadastro de dedução
   [ {descricao: 'Previdencia Privada', valor: 1000},
     {descricao: 'Funpresp', valor: 2000},
     {descricao: 'Parcela isenta de aposentadoria', valor: 4000}
@@ -48,6 +49,55 @@ describe 'Cadastro de dedução' do
     end
   end
 
+  # cadastro de previdencia oficial
+  describe 'quando é realizado um cadastro de contrinuição previdenciaria oficial' do
+    let(:descricaoPrevidenciaOficial) { 'Previdêcia Oficial' }
+    let(:valorPrevidenciaOficial) { 1000 }
+
+    before do 
+      irpf.cadastroPrevidenciaOficial(valorPrevidenciaOficial, descricaoPrevidenciaOficial)
+    end
+    
+    it 'a dedução total deve ser igual ao valor cadastrado' do
+      expect(irpf.totalDeducoes).to eq valorPrevidenciaOficial
+    end
+
+    it 'a lista de deduções declaradas deve conter o novo cadastro' do
+      expect(irpf.deducoesDeclaradas).to eq [descricaoPrevidenciaOficial]
+    end
+  end
+
+  # cadastro de pensão alimentícia
+  describe 'quando é realizado um cadastro de pensão alimentícia' do
+    let(:valorPensao) { 1000 }
+
+    before do
+      irpf.cadastroPensaoAlimenticia(valorPensao)
+    end
+
+    if 'a dedução total deve ser igual ao valor cadastrado' do
+      expect(irpf.totalDeducoes).to eq valorPensao
+    end
+  end
+
+  # cadastro de dependentes
+  describe 'quando é realizado o cadastro de dependentes' do
+    let(:nomeDependente) { 'Pedro Tiago e João' }
+    let(:dataDeNacimentoDependente) { '01/01/2011' }
+
+    before do
+      irpf.cadastroDependente(nomeDependente, dataDeNacimentoDependente)
+    end
+
+    if 'a dedução total deve ser igual ao (número de dependentes*189,59)' do
+      expect(irpf.totalDeducoes).to eq 189.59
+    end
+
+    if 'o nome do dependente deve estar na lista de dependentes' do
+      expect(irpf.listaDependentes).to eq [nomeDependente]
+    end
+  end
+  
 
 end
 

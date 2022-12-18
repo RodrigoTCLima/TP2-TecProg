d22964e5253d349195e176becb37dae89b4d5548
rodrigo <rodrigotiagocl@gmail.com>
2022-12-18 16:41:53 -0300

    Criação de teste de falsificação para calculo de aliquota efetiva
    Co-authored-by: Sergio Cipriano <sergiosacj@riseup.net>

---
+++ test/calculo_aliquota_efetiva.rb
@@ -0,0 +1,17 @@
+require './src/irpf'
+
+describe 'Calculo aliquota efetiva'
+    let(:irpf) { IRPF.new }
+
+    describe 'quando feito corretamente' do
+        before do
+            irpf.cadastroRendimento('Salario', 10000)
+            irpf.cadastroDeducao('Previdência Privada', 1000)
+            irpf.calculaTaxas()
+        end
+        it 'a aliquota efetiva deve ser a porcentagem do imposto sobre o rendimento total' do
+            expect(irpf.calculaAliquotaEfetiva).to eq 16.05
+        end
+    end
+
+end
